1e91c68548d54866f4f2dd8709ae92502cff7c17
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-29 19:07:01 -0400

    sales project first commit

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Alex", "Bessard", "alex@alex.com", 12155),
+    new SalesPerson("Toto", "Test", "toto@alex.com", 54745),
+    new SalesPerson("Jeanne", "Lebuche", "Jean@alex.com", 44775),
+    new SalesPerson("Sii", "ssdo", "sii@alex.com", 54575)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
